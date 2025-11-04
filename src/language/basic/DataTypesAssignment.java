df6188bc3d6b10d5d8c9d8ba40070fb5cf267a18
nehalS28 <nehal.sati@smartjoules.in>
2025-11-05 00:29:35 +0530

    datatypes

---
+++ src/language/basic/DataTypesAssignment.java
@@ -0,0 +1,113 @@
+package language.basic;
+
+public class DataTypesAssignment {
+
+	public static void main(String[] args) {
+		
+//		1. Temperature of a city in degrees Celsius: 25.5
+		float temp = 25.5f;
+		System.out.println(temp);
+		
+//		2. Whether a customer has placed an order: true or false
+		
+		boolean orderisplaced = true;
+		System.out.println(orderisplaced);
+		
+//		3. Person's phone number: "123-456-7890"
+		
+		String phonenumber = "123-456-7890";
+		System.out.println(phonenumber);
+		
+		
+//		4. Amount of money in a customer's bank account: 1000.50
+		
+		float money = 1000.50f;
+		System.out.println(money);
+		
+//		5. Person's email address: "john.doe@example.com"
+		
+		String email = "john.doe@example.com";
+		System.out.println(email);
+		
+//		6. Coordinates of a location (latitude, longitude): 37.7749, -122.4194
+		
+		float lat = 37.7749f;
+		float longi = -122.4194f;
+		
+		System.out.println("Latitude is "+ lat);
+		System.out.println("Longitude is "+ longi);
+		
+//		7. Person's marital status: true or false
+		
+		boolean mstatus = false;
+		System.out.println(mstatus);
+		
+//		8. Person's occupation: "Software Engineer"
+		
+		String occupation = "Software Engineer";
+		System.out.println(occupation);
+		
+//		9. Person's favourite colour: "Blue"
+		
+		String favcolor = "Blue";
+		System.out.println(favcolor);
+		
+//		10.Current year: 2023
+		
+		short year = 2023;
+		System.out.println(year);
+		
+//		11.Number of followers on a social media platform: 1,000,000
+		
+		int followers = 1000000;
+		System.out.println(followers);
+		
+//		12.Rating of a movie: 7.5
+		
+		float rating = 7.5f;
+		System.out.println(rating);
+		
+//		13.Person's blood type: 'A'
+		
+		char btype = 'A';
+		System.out.println(btype);
+		
+//		14.Title of a book: "To Kill a Mockingbird"
+		
+		String title = "To Kill a Mockingbird";
+		System.out.println("The title of the book is " + title);
+		
+//		15.Number of employees in a company: 500
+		
+		short employee_count = 500;
+		System.out.println( "Total headcount of the organisation "+ employee_count);
+		
+//		16.Time of an event: 2:30 PM
+		
+		String time = "2:30 PM";
+		System.out.println("The time of the event is " + time);
+		
+//		17.Name of a country: "United States"
+		
+		String name = "United States";
+		System.out.println("The name of the country is " + name );
+		
+//		18.Person's eye color: "Brown"
+		
+		String eye_color = "Brown";
+		System.out.println("My eye color is " + eye_color);
+		
+//		19.Person's birthplace: "New York City"
+		
+		String birthplace = "New York City";
+		System.out.println("The name of his birthplace is "+ birthplace);
+		
+//		20.Distance between two cities: 200.5
+		
+		float dist = 200.5f;
+		System.out.println("The distance between two cities is "+ dist +"KM");
+		
+		
+	}
+
+}
